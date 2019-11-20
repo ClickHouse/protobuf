c595109ebcc4277472577020e37942c2ea8790ac
filimonov <1549571+filimonov@users.noreply.github.com>
2019-11-20 09:40:07 +0100

    cmake - add pthreads explicitly
     to workaround https://github.com/ClickHouse/ClickHouse/issues/7114

--- cmake/protoc.cmake
+++ cmake/protoc.cmake
@@ -4,4 +4,9 @@ set(protoc_files
 
 add_executable(protoc ${protoc_files})
 target_link_libraries(protoc libprotobuf libprotoc)
+
+# workaround for https://github.com/ClickHouse/ClickHouse/issues/7114
+find_package(Threads)
+target_link_libraries(protoc Threads::Threads)
+
 add_executable(protobuf::protoc ALIAS protoc)
